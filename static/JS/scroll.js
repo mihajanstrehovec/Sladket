5f2e12a90f3910491d58fbd00b187e6e00bc6e33
Miha Jan Strehivec <48392708+mihajanstrehovec@users.noreply.github.com>
2021-02-11 17:48:00 +0100

    Update scroll.js

--- static/JS/scroll.js
+++ static/JS/scroll.js
@@ -1,1 +1,25 @@
-// ČE bo čas implementiraj funkcijo, ki bo glede na scroll premaknila ozadje zna bit lit
+function zoom(event) {
+    imgSc = document.getElementById("bgIMGscroll");
+    event.preventDefault();
+  
+     if (event.deltaY < 0) {
+       // Zoom in
+       scale -= event.deltaY * +4;
+     }
+     else {
+       // Zoom out
+       scale -= event.deltaY * +4;
+     }
+    
+  
+
+    // Restrict scale
+    //scale = Math.min(Math.max(.125, scale), 4);
+    //console.log()
+    // Apply scale transform
+    imgSc.style.transform = translateY(${scale}px);
+  }
+  
+  let scale = 1;
+  const el = document.querySelector('div');
+  document.onwheel = zoom;
