42829ef8fcb7e736d6a7a0fd4a8a6049c1b72f4d
Miha Jan Strehivec <48392708+mihajanstrehovec@users.noreply.github.com>
2021-02-11 19:30:12 +0100

    Update strankaController.php

--- controller/strankaController.php
+++ controller/strankaController.php
@@ -264,7 +264,7 @@ class strankaController {
                 $mail->Username   = 'mihec.strehci@gmail.com';                    
                 $mail->Password   = 'ritmecarji';     
                 $mail->Port = 587;                       
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         
+                #$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         
               
                 $mail->setFrom('sladkizob.info@gmail.com', 'sladket');
                 $mail->addAddress($data["mailStranke"]);     
